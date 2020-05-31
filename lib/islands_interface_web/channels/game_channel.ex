f1766f9d0172f483a8b2c1c981c8c57b3dbdde63
Douglas Moura <douuuglas@gmail.com>
2020-05-31 11:17:04 -0300

    exposing game on channel

--- lib/islands_interface_web/channels/game_channel.ex
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -14,4 +14,82 @@ defmodule IslandsInterfaceWeb.GameChannel do
   def join("game:" <> _player, _payload, socket) do
     {:ok, socket}
   end
+
+  # def handle_in("hello", payload, socket) do
+  #   {:reply, {:ok, payload}, socket}
+  # end
+
+  def handle_in("hello", payload, socket) do
+    broadcast!(socket, "said_hello", payload)
+    {:noreply, socket}
+  end
+
+  def handle_in("new_game", _payload, socket) do
+    "game:" <> player = socket.topic
+
+    case Game.start_link(player) do
+      {:ok, _pid} ->
+        {:reply, :ok, socket}
+
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
+  end
+
+  def handle_in("add_player", player, socket) do
+    case Game.add_player({:global, socket.topic}, player) do
+      :ok ->
+        broadcast!(socket, "player_added", %{message: "New player just joined: " <> player})
+        {:noreply, socket}
+
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
+  end
+
+  def handle_in("set_island_coordinates", payload, socket) do
+    %{"player" => player, "island" => island, "coordinates" => coordinates} = payload
+    player = String.to_atom(player)
+    island = String.to_atom(island)
+    coordinates = Enum.map(coordinates, fn coord -> String.to_atom(coord) end)
+
+    case Game.set_island_coordinates({:global, socket.topic}, player, island, coordinates) do
+      :ok -> {:reply, :ok, socket}
+      :error -> {:reply, :error, socket}
+    end
+  end
+
+  def handle_in("set_islands", player, socket) do
+    player = String.to_atom(player)
+
+    case Game.set_islands({:global, socket.topic}, player) do
+      :ok ->
+        broadcast!(socket, "player_set_islands", %{player: player})
+        {:noreply, socket}
+
+      :error ->
+        {:reply, :error, socket}
+    end
+  end
+
+  def handle_in("guess_coordinate", params, socket) do
+    %{"player" => player, "coordinate" => coordinate} = params
+    player = String.to_atom(player)
+    coordinate = String.to_atom(coordinate)
+
+    case Game.guess_coordinate({:global, socket.topic}, player, coordinate) do
+      {:hit, island,win} ->
+        result = %{hit: true, island: island, win: win}
+        broadcast!(socket, "player_guessed_coordinate", %{player: player, result: result})
+        {:noreply, socket}
+
+      {:miss, island, win} ->
+        result = %{hit: false, island: island, win: win}
+        broadcast!(socket, "player_guessed_coordinate", %{player: player, result: result})
+        {:noreply, socket}
+
+      {:error, reason} -> {:reply, {:error, %{player: player, reason: reason}}, socket}
+
+    end
+  end
 end
